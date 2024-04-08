81387257f876c57f9306d1a082ef10fe41be5e71
Kumar Aryan <aj619119@gmail.com>
2024-04-08 15:01:22 +0530

    make ammends

--- hawkscode.com/blog/xmlrpc0db0.php
+++ hawkscode.com/blog/xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://hawkscode.com/blog/</homePageLink>
+		<homePageLink>https://hawkscode.hopin.space/blog/</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://hawkscode.com/blog/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://hawkscode.com/blog/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://hawkscode.com/blog/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://hawkscode.com/blog/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://hawkscode.com/blog/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://hawkscode.hopin.space/blog/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://hawkscode.hopin.space/blog/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://hawkscode.hopin.space/blog/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://hawkscode.hopin.space/blog/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://hawkscode.hopin.space/blog/wp-json/" />
 			</apis>
 	</service>
 </rsd>
